db7912f0fbacfec45ebe17ad230dc15a6fccba08
HyeonseoJUNG <windymay1920@gmail.com>
2023-07-01 13:11:38 +0900

    Refactor: 커스텀 익셉션에 serialVersionUID 추가

--- src/main/java/com/project/toyland/common/error/exception/ApiException.java
+++ src/main/java/com/project/toyland/common/error/exception/ApiException.java
@@ -4,12 +4,17 @@ import com.project.toyland.common.error.code.ErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.io.Serial;
+
 /**
  * API 관련 custom exception
  */
 @Getter
 @RequiredArgsConstructor
 public class ApiException extends RuntimeException {
+
+	@Serial
+	private static final long serialVersionUID = 300497546254862376L;
 
 	/**
 	 * 정의해둔 에러 코드
